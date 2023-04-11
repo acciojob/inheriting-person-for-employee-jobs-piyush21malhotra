580be273240df0f087594db21b85cc241dcabd51
Piyush Malhotra <101010021+piyush21malhotra@users.noreply.github.com>
2023-04-12 00:22:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	constructor(name, age){
+		this.name = name
+		this.age = age
+	}
 
-function Employee(name, age, jobTitle) {}
+	greet() {
+		console.log("Hello, my name is , ", this.name ," I am ", this.age,  " years old.")
+	}
+}
+
+function Employee(name, age, jobTitle) extends Person{
+	super(name,age)
+	constructor(name, age, jobTitle){
+		this.name = super.name
+		this.age = super.age
+		this.jobTitle = jobTitle
+	}
+
+	jobGreet() {
+		console.log("Hello, my name is , ", super.name ," I am ", super.age,  " years old, and my job title is ", this.jobTitle)
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
