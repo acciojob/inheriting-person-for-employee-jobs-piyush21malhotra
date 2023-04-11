c13a00d1d99e82ab9633fcaef97121912e1887da
Piyush Malhotra <101010021+piyush21malhotra@users.noreply.github.com>
2023-04-12 00:48:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 // complete this js code
 function Person(name, age) {
-	constructor(name, age){
-		this.name = name
-		this.age = age
-	}
-
-	greet() {
-		console.log("Hello, my name is , ", this.name ," I am ", this.age,  " years old.")
-	}
+	this.name = name
+	this.age = age
 }
 
-function Employee(name, age, jobTitle) extends Person{
-	super(name,age)
-	constructor(name, age, jobTitle){
-		this.name = super.name
-		this.age = super.age
-		this.jobTitle = jobTitle
-	}
-
-	jobGreet() {
-		console.log("Hello, my name is , ", super.name ," I am ", super.age,  " years old, and my job title is ", this.jobTitle)
-	}
+Person.prototype.greet = function() {
+	console.log("Hello, my name is , ", this.name ," I am ", this.age,  " years old.")
 }
+
+function Employee(name, age, jobTitle){
+	Person.call(this, name, age)
+	this.jobTitle = jobTitle
+}
+
+Employee.prototype.jobGreet = function() {
+	console.log("Hello, my name is , ", super.name ," I am ", super.age,  " years old, and my job title is ", this.jobTitle)
+}
+
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
 
 // Do not change code below this line
 window.Person = Person;
